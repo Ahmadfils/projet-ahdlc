dda555f8aba615d6017e66a5557d89deccf1e61e
Abaone <ahmadfils257@gmail.com>
2025-05-18 14:51:26 +0200

    fix components

--- views/contact.php
+++ views/contact.php
@@ -1,30 +1,3 @@
-<div class="contact-section">
-    <div class="contact-image"></div>
-
-    <div class="contact-form">
-        <h3>Envoyez-nous un message</h3>
-        <form>
-            <div class="input-icon">
-                <input type="text" placeholder="Nom complet" required>
-            </div>
-            <div class="input-icon">
-                <input type="email" placeholder="Adresse e-mail" required>
-            </div>
-            <div class="input-icon">
-                <textarea placeholder="Votre message" required></textarea>
-            </div>
-            <button type="submit">Envoyer</button>
-        </form>
-    </div>
-</div>
-
-<section class="infos-pro">
-    <h2>Nos coordonnées</h2>
-    <p>© 2025 Inzuex - Tous droits réservés.</p>
-    <p>Contact : info@inzuex.com | Tél : +257 123 456 789</p>
-    <p>Adresse : Bujumbura, Burundi</p>
-</section>
-
 <section class="faq-section">
     <h2>FAQ - Questions Fréquentes</h2>
 
@@ -55,3 +28,33 @@
             de bénévolat ou de partenariat.</div>
     </div>
 </section>
+
+<section class="infos-pro">
+    <h2>Nos coordonnées</h2>
+    <p>© 2025 Inzuex - Tous droits réservés.</p>
+    <p>Contact : info@inzuex.com | Tél : +257 123 456 789</p>
+    <p>Adresse : Bujumbura, Burundi</p>
+</section>
+
+
+<div class="contact-section">
+    <div class="contact-image"></div>
+
+    <div class="contact-form">
+        <h3>Envoyez-nous un message</h3>
+        <form>
+            <div class="input-icon">
+                <input type="text" placeholder="Nom complet" required>
+            </div>
+            <div class="input-icon">
+                <input type="email" placeholder="Adresse e-mail" required>
+            </div>
+            <div class="input-icon">
+                <textarea placeholder="Votre message" required></textarea>
+            </div>
+            <button type="submit">Envoyer</button>
+        </form>
+    </div>
+</div>
+
+
