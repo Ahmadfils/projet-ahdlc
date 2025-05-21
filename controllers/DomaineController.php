b1ed9e5b448060129195cf6425939a86b06d937d
Abaone <ahmadfils257@gmail.com>
2025-05-21 13:22:43 +0200

    admin folder

--- controllers/DomaineController.php
+++ controllers/DomaineController.php
@@ -1,5 +1,5 @@
 <?php
-class DomainController extends Controller {
+class DomaineController extends Controller {
     public function index() {
         $data = [
             'title' => 'Nos Domaines',
